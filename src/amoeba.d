ca45bbca268dafa045a1a1f6ff72d6e49d9840f1
Richard Delorme <edax.reversi@gmail.com>
2017-02-04 00:09:09 +0100

    Unittest support

--- src/amoeba.d
+++ src/amoeba.d
@@ -11,13 +11,16 @@ import std.stdio;
 void main(string[] arg) {
 	kpk.init();
 
-	if (arg.length > 1) {
-		if (arg[1] == "perft") perft(arg[1 .. $], null);
-		else if (arg[1] == "bench") epdTest(arg[1 .. $], false);
-		else if (arg[1] == "epd") epdTest(arg[1 .. $], true);
-	} else {
-		Uci uci = new Uci;
-		uci.loop();
+	version (unittest) {}
+	else {
+		if (arg.length > 1) {
+			if (arg[1] == "perft") perft(arg[1 .. $], null);
+			else if (arg[1] == "bench") epdTest(arg[1 .. $], false);
+			else if (arg[1] == "epd") epdTest(arg[1 .. $], true);
+		} else {
+			Uci uci = new Uci;
+			uci.loop();
+		}
 	}
 }
 
